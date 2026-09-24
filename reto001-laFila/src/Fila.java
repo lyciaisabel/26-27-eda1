4019784fda0fc29f6fd6fe83e6dbd25d5eaa0456
lyciaisabel <lysapaulo07@gmail.com>
2026-09-24 13:00:26 +0200

    Corrige Fila.java: agrega metodos obtener y removerEnIndice

--- reto001-laFila/src/Fila.java
+++ reto001-laFila/src/Fila.java
@@ -1,62 +1,93 @@
-import java.util.LinkedList;
-import java.util.Random;
-
 public class Fila {
 
-    private final LinkedList<Persona> personas = new LinkedList<>();
+    private Persona[] personas;
+    private int tamano;
     private final int capacidadMaxima;
 
     public Fila(int capacidadMaxima) {
         this.capacidadMaxima = capacidadMaxima;
+        this.personas = new Persona[16];
+        this.tamano = 0;
+    }
+
+    private void asegurarCapacidad() {
+        if (tamano == personas.length) {
+            Persona[] nuevo = new Persona[personas.length * 2];
+            for (int i = 0; i < tamano; i++) {
+                nuevo[i] = personas[i];
+            }
+            personas = nuevo;
+        }
     }
 
     public boolean estaLlena() {
-        return personas.size() >= capacidadMaxima;
+        return tamano >= capacidadMaxima;
     }
 
     public boolean estaVacia() {
-        return personas.isEmpty();
+        return tamano == 0;
     }
 
     public int tamano() {
-        return personas.size();
+        return tamano;
     }
 
     public void agregarAlFinal(Persona persona) {
-        personas.addLast(persona);
+        asegurarCapacidad();
+        personas[tamano] = persona;
+        tamano++;
     }
 
     public void agregarAlFrente(Persona persona) {
-        personas.addFirst(persona);
+        agregarEnPosicion(persona, 0);
     }
 
     public void agregarDespuesDe(Persona persona, int indice) {
-        personas.add(indice + 1, persona);
+        agregarEnPosicion(persona, indice + 1);
+    }
+
+    private void agregarEnPosicion(Persona persona, int posicion) {
+        asegurarCapacidad();
+        for (int i = tamano; i > posicion; i--) {
+            personas[i] = personas[i - 1];
+        }
+        personas[posicion] = persona;
+        tamano++;
     }
 
     public int indiceUltimoPreferente() {
         int indice = -1;
-        for (int i = 0; i < personas.size(); i++) {
-            if (personas.get(i).isPreferente()) {
+        for (int i = 0; i < tamano; i++) {
+            if (personas[i].isPreferente()) {
                 indice = i;
             }
         }
         return indice;
     }
 
-    public int indiceAleatorio(Random random) {
-        return random.nextInt(personas.size());
+    public int indiceAleatorio() {
+        return (int) (Math.random() * tamano);
     }
 
     public Persona atenderFrente() {
-        return personas.pollFirst();
+        Persona persona = personas[0];
+        for (int i = 0; i < tamano - 1; i++) {
+            personas[i] = personas[i + 1];
+        }
+        personas[tamano - 1] = null;
+        tamano--;
+        return persona;
     }
 
-    public boolean removerPersona(Persona persona) {
-        return personas.remove(persona);
+    public void removerEnIndice(int indice) {
+        for (int i = indice; i < tamano - 1; i++) {
+            personas[i] = personas[i + 1];
+        }
+        personas[tamano - 1] = null;
+        tamano--;
     }
 
-    public LinkedList<Persona> getPersonas() {
-        return personas;
+    public Persona obtener(int indice) {
+        return personas[indice];
     }
 }
